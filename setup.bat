a3f278e3673fda3d1a2db8f9b4cb30c1d2a5a122
Md Noukhej <mnoukhej@gmail.com>
2025-02-03 17:33:35 +0530

    Uploaded .gitignore

--- setup.bat
+++ setup.bat
@@ -1,9 +1,11 @@
 @echo off
+
 python -m venv .venv
 echo .venv created
 
+
 call .venv\Scripts\activate.bat
-echo.venv activated.
+echo .venv activated.
 
 python.exe -m pip install --upgrade pip
 echo pip upgrade -- done
@@ -15,6 +17,15 @@ echo requirements installed
 cd school_project
 python manage.py makemigrations
 python manage.py migrate
+
+
+echo Creating .gitignore file...
+
+echo .venv/ > .gitignore
+:: Create .gitignore and add the .venv directory
+echo .gitignore file created.
+
 echo.
 echo Setup complete! Now you can run the server using run_server.bat
+
 pause
